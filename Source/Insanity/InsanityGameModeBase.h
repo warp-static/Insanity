0988a6f68db32032088ebe690c01107d36f97bb5
warp-static <rdorman34@gmail.com>
2018-10-20 18:15:08 +0100

    blank c++ project

---
+++ Source/Insanity/InsanityGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InsanityGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INSANITY_API AInsanityGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
